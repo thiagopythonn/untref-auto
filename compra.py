b5099c33e027b88e55589a05af2235ae1f93dc03
thiagopythonn <thoagpirdam@gmail.com>
2024-11-05 02:13:02 +0100

    caso 2 y 3

---
+++ compra.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Firefox()
+driver.implicitly_wait(5)
+driver.get("https://www.saucedemo.com/")
+driver.find_element(By.ID,'user-name').send_keys("standard_user")
+driver.find_element(By.ID,"password").send_keys("secret_sauce")
+driver.find_element(By.ID,"login-button").click()
+driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack").click()
+driver.find_element(By.ID,"shopping_cart_container").click()
+driver.find_element(By.ID,"remove-sauce-labs-backpack").click()
+driver.find_element(By.ID,"continue-shopping").click()
+driver.find_element(By.ID,"add-to-cart-sauce-labs-bike-light").click()
+driver.find_element(By.ID,"add-to-cart-sauce-labs-bolt-t-shirt").click()
+driver.find_element(By.ID,"shopping_cart_container").click()
+driver.find_element(By.ID,"checkout").click()
+driver.find_element(By.ID,"first-name").send_keys("thiago")
+driver.find_element(By.ID,"last-name").send_keys("thiago")
+driver.find_element(By.ID,"postal-code").send_keys("5000")
+driver.find_element(By.ID,"continue").click()
+driver.find_element(By.ID,"finish").click()
---
+++ postalcode.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Firefox()
+driver.implicitly_wait(5)
+driver.get("https://www.saucedemo.com/")
+driver.find_element(By.ID,'user-name').send_keys("standard_user")
+driver.find_element(By.ID,"password").send_keys("secret_sauce")
+driver.find_element(By.ID,"login-button").click()
+driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack").click()
+driver.find_element(By.ID,"add-to-cart-sauce-labs-bike-light").click()
+driver.find_element(By.ID,"add-to-cart-sauce-labs-bolt-t-shirt").click()
+driver.find_element(By.ID,"add-to-cart-sauce-labs-fleece-jacket").click()
+driver.find_element(By.ID,"add-to-cart-sauce-labs-onesie").click()
+driver.find_element(By.ID,"add-to-cart-test.allthethings()-t-shirt-(red)").click()
+driver.find_element(By.ID,"shopping_cart_container").click()
+driver.find_element(By.ID,"checkout").click()
+driver.find_element(By.ID,"first-name").send_keys("thiago")
+driver.find_element(By.ID,"continue").click()
+driver.find_element(By.ID,"last-name").send_keys("thiago")
+driver.find_element(By.ID,"continue").click()
